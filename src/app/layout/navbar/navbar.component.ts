0f585703c225395a45acab16c86b67fb4e8bafb1
Fernando Hernández Lázaro <ferherlaz@gmail.com>
2026-05-09 06:22:22 +0200

    menu burguer ok

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -9,9 +9,18 @@ import { LanguageService } from '../../core/services/language.service';
 export class NavbarComponent {
   public languageService = inject(LanguageService);
   isScrolled = false;
+  isMenuOpen = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 50;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
   }
 }
